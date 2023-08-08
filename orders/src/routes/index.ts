4169536ca809e7797a81d4c2a48686195790d77f
jtopanotti <jtopanotti@ciandt.com>
2023-08-08 15:28:49 -0300

    Adding orders service

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,12 +1,9 @@
 import express, { Request, Response } from 'express';
-import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.get('/api/tickets', async (req: Request, res: Response) => {
-  const tickets = await Ticket.find({});
-
-  res.send(tickets);
+router.get('/api/orders', async (req: Request, res: Response) => {
+    res.send({});
 });
 
-export { router as indexTicketRouter };
+export { router as indexOrderRouter };
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,17 +1,9 @@
 import express, { Request, Response } from 'express';
-import { NotFoundError } from '@somethingorg/common';
-import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.get('/api/tickets/:id', async (req: Request, res: Response) => {
-  const ticket = await Ticket.findById(req.params.id);
-
-  if (!ticket) {
-    throw new NotFoundError();
-  }
-
-  res.send(ticket);
+router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
+    res.send({});
 });
 
-export { router as showTicketRouter };
+export { router as showOrderRouter };
